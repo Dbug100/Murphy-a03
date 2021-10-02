090c2cab81d3c41713d4a5c61c3a352d43b97bbb
deaja <deaja214@gmail.com>
2021-10-02 18:44:41 -0400

    exercise25 solution

--- exercise25/src/main/java/baseline/Solution25.java
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -6,38 +6,97 @@ package baseline;
  */
 
 
+import java.util.*;
+import java.util.Scanner;
+
 public class Solution25 {
     public static void main(String[] args){
+        Scanner userIn = new Scanner(System.in);
         //take user input string
+        System.out.print("Enter your password:");
+        String pass = userIn.nextLine();
 
         /*  get return value from passwordValidator
             if equal 1 print..... if equal 2 print...etc
         */
+        if(passwordValidator(pass)==0){
+            System.out.print("The password '" +pass +"' is a very weak password.");
+        }
+        else if(passwordValidator(pass)==1){
+            System.out.print("The password '" +pass +"' is a weak password.");
+        }
+        else if(passwordValidator(pass)==2){
+            System.out.print("The password '" +pass +"' is a strong password.");
+        }
+        else if(passwordValidator(pass)==3){
+            System.out.print("The password '" +pass +"' is a very strong password.");
+        }
+        else if(passwordValidator(pass)==4){
+            System.out.print("The password '" +pass +"' is an unknown strength.");
+        }
 
     }
 
-    private static int passwordValidator(String input){
+    private static int passwordValidator(String pass){
         // check length of string
+        //use check case built-in functions to find letters and use hasDigit for numbers
+        int len = pass.length();
+        boolean lowLet = false;
+        boolean upLet = false;
+        boolean num = false;
+        boolean special = false;
+        char c ;
         //if less than 8
-        //check if String contains letters
-        //check if string contains numbers
-
+        // check if String contains letters
+        // check if string contains numbers
+        if(len < 8){
+            for(int i = 0; i < len; i++){
+                c = pass.charAt(i);
+                if (Character.isUpperCase(c)) {
+                    upLet = true;}
+                if (Character.isLowerCase(c)) {
+                    lowLet = true;}
+                if (Character.isDigit(c)) {
+                    num = true;}
+            }
+            if(num && !upLet && !lowLet){
+                return 0;}
+            else if(!num && (upLet || lowLet)){
+                return 1;}
+            else{
+                return 4;}
+        }
         // return 0 for very weak
-        return 0;
         //return 1 for weak password
-
-        //if greater than or equal to 8
+        // if greater than or equal to 8
         //check if String contains letters
         //check if String contains numbers
         //check if String contains special characters
-        // if contains only letters and numbers
+        //if contains only letters and numbers
         //return 2 for strong password
-
+        Set<Character> specialChar = new HashSet<Character>(Arrays.asList('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+'));
+        for(int i = 0; i < len; i++){
+            c = pass.charAt(i);
+            if (Character.isUpperCase(c)) {
+                upLet = true;}
+            if (Character.isLowerCase(c)) {
+                lowLet = true;}
+            if (Character.isDigit(c)) {
+                num = true;}
+            if(specialChar.contains(c)){
+                special=true;}
+        }
         //if greater than or equal to 8
         //check if String contains letters
         //check if String contains numbers
         //check if String contains special characters
         //return 3 for very strong password
+        if((upLet || lowLet) && num && !special){
+            return 2;}
+        else if((upLet || lowLet) && num && special){
+            return 3;}
+        else{
+            return 4;}
 
         // else
         // return 4 for unknown strength
---
+++ exercise25/src/test/java/baseline/Solution25Test.java
@@ -0,0 +1,55 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution25Test {
+
+    @Test
+    void main() {
+        Solution25 test = new Solution25();
+        String pass = "12345";
+    }
+}
+class Solution25Test2 {
+
+    @Test
+    void main() {
+        Solution25 test = new Solution25();
+        String pass = "abcdef";
+    }
+}
+class Solution25Test3 {
+
+    @Test
+    void main() {
+        Solution25 test = new Solution25();
+        String pass = "abc123xyz";
+    }
+}
+class Solution25Test4 {
+
+    @Test
+    void main() {
+        Solution25 test = new Solution25();
+        String pass = "1337h@xor!";
+    }
+}
+class Solution25Test5 {
+
+    @Test
+    void main() {
+        Solution25 test = new Solution25();
+        String pass = "12abc";
+    }
+}
+
+class Solution25Test6 {
+
+    @Test
+    void main() {
+        Solution25 test = new Solution25();
+        String pass = "12ab@";
+    }
+}
