580ed7137ff14c6c47b88c8dafc9cd9ecaf1c4eb
deaja <deaja214@gmail.com>
2021-10-02 13:50:21 -0400

    exercise 24 logic

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,19 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Deaja Murphy
+ */
+
+public class Solution24 {
+}
+
+
+/*
+    An anagram is a word or phrase that is formed by
+    rearranging the letters of another word or phrase
+
+    words or phrases must be same length
+    check to see if they contain the same letters
+    must be same case --- sort letters instead of looping through both
+    check if equal
+ */
