b7cd15fd8e4d0996047ff303888e4e9f7a0d376f
deaja <deaja214@gmail.com>
2021-10-03 14:38:52 -0400

    exercise 27 pseudocode

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,43 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Deaja Murphy
+ */
+
+public class Solution27 {
+    public static void main(String[] args){
+        //ask user for needed inputs
+    }
+
+    private static boolean firstName(){
+        //if input.lenght < 2 false else true
+        return true;
+    }
+
+    private static boolean lastName(){
+        //if input.lenght < 2 false else true
+        return true;
+    }
+
+    private static boolean idFormat(){
+        //use java built in pattern matcher
+        // if it matches true if not false
+        return true;
+    }
+
+    private static boolean zip(){
+        //if zip length != 5 false else true
+        return true;
+    }
+
+    private static void errorCheck(){
+        //if !zip, !firstName, !lastName, !idFormat
+        //print no error statement
+        //if !zip print...
+        //if !firstName print...
+        //if !lastName print...
+        //if !idFormat print...
+    }
+
+}
