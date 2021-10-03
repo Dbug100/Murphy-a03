95419185b99bde042cadd18591755259d699b095
deaja <deaja214@gmail.com>
2021-10-03 17:01:07 -0400

    exercise29 psudeocode

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,18 @@
+package baseline;
+
+public class Solution29 {
+    public static void main(String[] args){
+        //while (1)
+        //ask for return rate
+        //convert String input to int
+        // 72/input = r
+        //print output
+        //number exception to stop inputs of letters
+        //print error message
+        //arithmetic exception to find input of 0
+        //print error message
+        //if no errors found break
+
+
+    }
+}
