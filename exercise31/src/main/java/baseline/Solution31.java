3afbd8b21bccbe4946deac45289c9af243927e7c
deaja <deaja214@gmail.com>
2021-10-03 19:13:07 -0400

    exercise 31 solution

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -1,12 +1,39 @@
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution31 {
     public static void main(String[] args){
+        Scanner userIn = new Scanner(System.in);
+        int errors = 0, hR=0, age=0, target ;
         //get user heart rate and age as ints
-        //throw exception and ask for input again if not int
+        while(true) {
+            try {
+                System.out.print("Enter heart rate:");
+                String hRate = userIn.nextLine();
+                System.out.print("Enter age:");
+                String userAge = userIn.nextLine();
+                hR = Integer.parseInt(hRate);
+                age = Integer.parseInt(userAge);
+                System.out.println("Resting Pulse: " + hR + " Age: " + age);
+                errors++;
+            }
+            catch (NumberFormatException e) {
+                //throw exception and ask for input again if not int
+                System.out.println("Sorry. Please input a number.");
+            }
+            if(errors!=0)
+                break;
+        }
         //print top of table
+        System.out.println("Intensity\t | \tRate");
+        System.out.println("_____________|____________");
         //for loop i <= 95 -- i + 5
-        //target heart rate equation
-        //print values as value tab | tab value 
+        for(int i = 55; i<=95; i+=5){
+            //target heart rate equation
+            target = (((220 - age) - hR) * i) + hR;
+            //print values as value tab | tab value
+            System.out.println(i+"%\t\t\t |"+target +"bpm");
+        }
     }
 }
---
+++ exercise31/src/test/java/baseline/Solution31Test.java
@@ -0,0 +1,42 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution31Test {
+
+    @Test
+    void main() {
+        Solution31 test = new Solution31();
+        String hRate = "HRt";
+        String userAge = "old";
+    }
+}
+class Solution31Test2 {
+
+    @Test
+    void main() {
+        Solution31 test = new Solution31();
+        String hRate = "";
+        String userAge = "19";
+    }
+}
+class Solution31Test3 {
+
+    @Test
+    void main() {
+        Solution31 test = new Solution31();
+        String hRate = "80";
+        String userAge = "";
+    }
+}
+class Solution31Test4 {
+
+    @Test
+    void main() {
+        Solution31 test = new Solution31();
+        String hRate = "80";
+        String userAge = "19";
+    }
+}
