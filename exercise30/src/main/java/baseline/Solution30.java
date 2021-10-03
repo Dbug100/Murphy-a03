40c9f66dd581bb34d7e2a5ec4552884651766681
deaja <deaja214@gmail.com>
2021-10-03 17:56:11 -0400

    exercise30 solution

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -3,7 +3,13 @@ package baseline;
 public class Solution30 {
     public static void main(String[] args){
         //nested for loop both 1 <= 12
-        //print loop 1 * loop 2
-        //print new line after first loop 
+        for(int i = 1; i <= 12;i++){
+            for(int j = 1; j <= 12; j++){
+                //print loop 1 * loop 2
+                System.out.print((i*j)+"\t");
+            }
+            //print new line after first loop
+            System.out.print("\n");
+        }
     }
 }
