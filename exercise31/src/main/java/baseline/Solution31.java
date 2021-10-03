a7b3db6239849869df46be1752e45fd11aa21584
deaja <deaja214@gmail.com>
2021-10-03 18:13:36 -0400

    exercise 31 pseudocode

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,12 @@
+package baseline;
+
+public class Solution31 {
+    public static void main(String[] args){
+        //get user heart rate and age as ints
+        //throw exception and ask for input again if not int
+        //print top of table
+        //for loop i <= 95 -- i + 5
+        //target heart rate equation
+        //print values as value tab | tab value 
+    }
+}
