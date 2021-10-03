8a0ddbf8697016394117f597ea1a8153bc4eb86a
deaja <deaja214@gmail.com>
2021-10-03 19:57:37 -0400

    accept non numerical input as incorrect guesses

--- exercise32/src/main/java/baseline/Solution32.java
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -1,5 +1,6 @@
 package baseline;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Solution32 {
@@ -8,32 +9,38 @@ public class Solution32 {
         int secretNumber, guesses = 0;
         // ask for difficulty
         while (true) {
-            System.out.print("Enter the difficulty level (1, 2, or 3): ");
-            int level = userIn.nextInt();
-            //if difficulty 1-3 generate number for difficulty
-            if (level == 1)
-                secretNumber = (int) (Math.random() * 10 + 1);
-            else if (level == 2)
-                secretNumber = (int) (Math.random() * 100 + 1);
-            else
-                secretNumber = (int) (Math.random() * 1000 + 1);
-            //ask for guess
-            System.out.print("I have my number. What's your guess?: ");
-            int guess = userIn.nextInt();
-            guesses++;
-            //while user has not guessed correctly
-            while (guess != secretNumber) {
-                //if < number print too low
-                if(guess<secretNumber) {
-                    System.out.print("Too low. Guess again: ");
-                    guess = userIn.nextInt();
-                }
-                //if > number print too high
-                if(guess<secretNumber) {
-                    System.out.print("Too high. Guess again: ");
-                    guess = userIn.nextInt();
-                }
+
+                System.out.print("Enter the difficulty level (1, 2, or 3): ");
+                int level = userIn.nextInt();
+                //if difficulty 1-3 generate number for difficulty
+                if (level == 1)
+                    secretNumber = (int) (Math.random() * 10 + 1);
+                else if (level == 2)
+                    secretNumber = (int) (Math.random() * 100 + 1);
+                else
+                    secretNumber = (int) (Math.random() * 1000 + 1);
+                //ask for guess
+                System.out.print("I have my number. What's your guess?: ");
+                try {
+                    int guess = userIn.nextInt();
                 guesses++;
+                //while user has not guessed correctly
+                while (guess != secretNumber) {
+                    //if < number print too low
+                    if (guess < secretNumber) {
+                        System.out.print("Too low. Guess again: ");
+                        guess = userIn.nextInt();
+                    }
+                    //if > number print too high
+                    if (guess < secretNumber) {
+                        System.out.print("Too high. Guess again: ");
+                        guess = userIn.nextInt();
+                    }
+                    guesses++;
+                }
+            }
+            catch (InputMismatchException nfe){
+                System.out.print("Non numerical input. Guess again: ");
             }
             //out of while print congrats
             System.out.println("You got it in "+guesses +" guesses!");
