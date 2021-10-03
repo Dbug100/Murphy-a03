b98e2c501a7f6dfa90fe7d086126364b9035fcc6
deaja <deaja214@gmail.com>
2021-10-03 17:49:38 -0400

    exercise 30 pseudocode

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,9 @@
+package baseline;
+
+public class Solution30 {
+    public static void main(String[] args){
+        //nested for loop both 1 <= 12
+        //print loop 1 * loop 2
+        //print new line after first loop 
+    }
+}
