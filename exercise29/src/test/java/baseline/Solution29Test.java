594562a2aea66f9d56b5a59e20a89df802a0ac7a
deaja <deaja214@gmail.com>
2021-10-03 17:33:51 -0400

    exercise29 unit testing

---
+++ exercise29/src/test/java/baseline/Solution29Test.java
@@ -0,0 +1,38 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution29Test {
+
+    @Test
+    void main() {
+        Solution29 test = new Solution29();
+        String user = "0";
+    }
+}
+class Solution29Test2 {
+
+    @Test
+    void main() {
+        Solution29 test = new Solution29();
+        String user = "ABCDE";
+    }
+}
+class Solution29Test3 {
+
+    @Test
+    void main() {
+        Solution29 test = new Solution29();
+        String user = "";
+    }
+}
+class Solution29Test4 {
+
+    @Test
+    void main() {
+        Solution29 test = new Solution29();
+        String user = "12";
+    }
+}
