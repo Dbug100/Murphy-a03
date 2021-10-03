4f1f3e7d2c665a711fe23c9b2d63334ec21390aa
deaja <deaja214@gmail.com>
2021-10-03 15:45:00 -0400

    exercise 28 solution

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,21 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution28 {
+    public static void main(String[] args){
+        Scanner userIn = new Scanner(System.in);
+        //varaible to hold total
+        int sum = 0;
+        //for loop that runs 5 times
+        for(int i = 0; i<5; i++){
+            //print to ask for number
+            System.out.print("Enter a number: ");
+            int user = userIn.nextInt();
+            //add numbers
+            sum += user;
+        }
+        //print total
+        System.out.print("The total is "+sum);
+    }
+}
