45c7e171a0ac5d30b8d609382b5376fa53666b2e
deaja <deaja214@gmail.com>
2021-10-03 19:27:09 -0400

    exercise32 psuedocode

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,24 @@
+package baseline;
+
+public class Solution32 {
+    public static void main(String[] args){
+
+        // ask for difficulty
+
+        //if difficulty 1-3 generate number for difficulty
+
+        //ask for guess
+
+        //while user has not guessed correctly
+
+        //if < number print too low
+
+        //if > number print too high
+
+        //out of while print congrats
+
+        //ask to play again reset values
+        
+    }
+
+}
