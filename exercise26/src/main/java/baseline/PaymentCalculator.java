1a2ac073309ddf906de5044a3c2c4de906ac4236
deaja <deaja214@gmail.com>
2021-10-02 20:09:57 -0400

    exercise 26 solution

--- exercise26/src/main/java/baseline/PaymentCalculator.java
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -1,8 +1,13 @@
 package baseline;
 
 public class PaymentCalculator extends Solution26 {
-    public static void calculateMonthsUntilPaidOff(){
+
+    public int calculateMonthsUntilPaidOff(){
+        Solution26 calc = new Solution26();
         //user apr / 365
+        calc.apr = calc.apr/365;
         //set int equal to (int) Math.ceil(-(1/30) * Math.log(1 + balance/payment * (1 - Math.pow(1 + apr,30))) / Math.log(1 + apr))
+        int months = (int) Math.ceil((-1 / 30.0) * Math.log(1 + balance / payment * (1 - Math.pow(1 + apr, 30))) / Math.log(1 + apr));
+        return months;
     }
 }
--- exercise26/src/main/java/baseline/Solution26.java
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -5,8 +5,28 @@ package baseline;
  *  Copyright 2021 Deaja Murphy
  */
 
+import java.util.Scanner;
+
 public class Solution26 {
+    static Double apr;
+    static Double balance;
+    static Double payment;
     public static void main(String[] args){
+        Scanner userIn = new Scanner(System.in);
         //ask user for needed values for calculation
+        System.out.print("What is your balance? ");
+        balance = userIn.nextDouble();
+
+        System.out.print("What is the APR on the card (as a percent)? ");
+        apr = userIn.nextDouble();
+        apr = apr / 100;
+
+        PaymentCalculator calc = new PaymentCalculator();
+        System.out.print("What is the monthly payment you can make? ");
+        payment = userIn.nextDouble();
+
+        int months = calc.calculateMonthsUntilPaidOff();
+        System.out.print("It will take you "+months+" to pay off this card.");
+
     }
 }
