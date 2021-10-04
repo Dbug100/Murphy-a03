0e28863fb59c98ec392f199c592db509e4fb8c3d
deaja <deaja214@gmail.com>
2021-10-03 23:58:52 -0400

    exercise 37 psuedocode

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,25 @@
+package baseline;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Deaja Murphy
+ */
+public class Solution37 {
+    public static void main(String[] args){
+        Scanner userIn = new Scanner(System.in);
+        //get user input
+
+        //call makePassword
+    }
+    private static void makePassword(int length, int special, int numNum){
+        //make list of letters, list of specials, and list of nums
+        //make array to store password
+        //use rand nums to pick random specials and store in array
+        //repeat for numbers
+        //fill rest of array with letters
+        // java shuffle method
+        //print password
+    }
+}
