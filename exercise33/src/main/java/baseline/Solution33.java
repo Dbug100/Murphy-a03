379c9ebcb40481f30ab2c8744b5501ac72f3b18e
deaja <deaja214@gmail.com>
2021-10-03 20:14:24 -0400

    exercise 33 psedocode and header for other files

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,10 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Deaja Murphy
+ */
+public class Solution33 {
+    String[] response = {"Yes", "No", "Ask Again Later"};
+    //generate random number 0,1, or 2
+    //print item in array at position of generated num
+}
