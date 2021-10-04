51ba2199fcb3f4dc117fb3966e2a9f916bb39b76
deaja <deaja214@gmail.com>
2021-10-03 22:00:58 -0400

    exercise 35 solution

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -4,20 +4,30 @@ package baseline;
  *  Copyright 2021 Deaja Murphy
  */
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Solution35 {
     public static void main(String[] args){
+        Scanner userIn = new Scanner(System.in);
         //initialize array
-
+        ArrayList<String> names = new ArrayList<String>();
         //while loop
-
-        //ask to enter name
-
-        //accept name until blank entry
-
-        //exit loop at blank entry
+        while (true){
+            //ask to enter name
+            System.out.println("Enter a name: ");
+            String nameIn = userIn.nextLine();
+            //accept name until blank entry
+            if(nameIn.isBlank() || nameIn.isEmpty())
+                break; //exit loop at blank entry
+            else
+                names.add(nameIn);
+            }
 
         //generate random number between 0 and length of array
-
+        int len = names.size();
+        int index = (int) Math.floor(Math.random() * (len + 1));
         //print name at that index
+        System.out.print("The winner is... "+ names.get(index));
+        }
     }
-}
