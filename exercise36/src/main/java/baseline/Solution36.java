8071b9501cdcdf996e97e6e5d380c5a0e7a892ab
deaja <deaja214@gmail.com>
2021-10-03 23:42:01 -0400

    exercise 36 solution

--- exercise36/src/main/java/baseline/Solution36.java
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -1,56 +1,102 @@
 package baseline;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Solution36 {
     public static void main(String[] args){
+        Scanner userIn = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
         //ask for numbers in a while loop
-
-        //if entry does not equal done NumberFormatException
-        //if not done and is a number add to list
-
-        //when user enters done exit loop
-
+        while(true) {
+            //if entry does not equal done NumberFormatException
+            //if not done and is a number add to list
+            try {
+                System.out.print("Enter a number: ");
+                String user = userIn.nextLine();
+                //when user enters done exit loop
+                if(user.equals("done")){
+                    break;
+                }
+                int num = Integer.parseInt(user);
+                numbers.add(num);
+            }
+            catch(NumberFormatException nfe){
+                //ignore input
+            }
+        }
         //call average
+        double avg = average(numbers);
         //call min
+        int mini = min(numbers);
         //call max
-        //call std
+        int maxi = max(numbers);
         //call printOut(average, min, max, std)
+        printOut(numbers, avg, mini, maxi);
     }
     private static double average(ArrayList<Integer> array){
+        int sum = 0;
         //for length of list
-        //add each num together
+        for (Integer integer : array) {
+            //add each num together
+            sum += integer;
+        }
         //divide by size
         //return
-        return 0;
+        return (double)(sum)/array.size();
     }
-    private static int min(ArrayList<Integer> array){
+    private static int min(ArrayList<Integer> array) {
         // int = 0
+        int num = array.get(0);
         //for length of array
-        // if int > array[index]
-        //int = array[index]
+        for (Integer integer : array) {
+            // if int > array[index]
+            if (num > integer)
+                num = integer;//int = array[index]
+        }
         //return
-        return 0;
+        return num;
     }
     private static int max(ArrayList<Integer> array){
         // int = 0
+        int num = 0;
         //for length of array
-        // if int < array[index]
-        //int = array[index]
+        for (Integer integer : array) {
+            // if int < array[index]
+            if (num < integer)
+                num = integer;//int = array[index]
+        }
         //return
-        return 0;
+        return num;
     }
     private static double std(ArrayList<Integer> array){
+        double avg = average(array);
+        double sum = 0;
         // for length of array
-        // sum+= math.pow((array[index]-average),2)
+        for (Integer integer : array) {
+            // sum+= math.pow((array[index]-average),2)
+            sum += Math.pow((integer - avg), 2);
+        }
         //double stdv = Math.sqrt(sum/array size)
         //return
-        return 0;
+        return Math.sqrt(sum/array.size());
     }
-    private static void printOut(double avg, int minimum, int maximum, double stdv){
+    private static void printOut(ArrayList<Integer> array, double avg, int minimum, int maximum){
+        System.out.print("Numbers: ");
+        for(int i = 0; i < array.size();i++){
+            System.out.print(array.get(i));
+            if(i != array.size()-1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
         //print avg
+        System.out.println("The average is "+avg);
         //print min
+        System.out.println("The minimum is "+minimum);
         //print max
+        System.out.println("The maximum is "+maximum);
         //print stdv
+        System.out.println("The standard deviation is "+String.format("%.2f",std(array)));
     }
 }
---
+++ exercise36/src/test/java/baseline/Solution36Test.java
@@ -0,0 +1,38 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution36Test {
+
+    @Test
+    void main() {
+        Solution36 test = new Solution36();
+        String user = "100";
+    }
+}
+class Solution36Test2 {
+
+    @Test
+    void main() {
+        Solution36 test = new Solution36();
+        String user = "bop";
+    }
+}
+class Solution36Test3 {
+
+    @Test
+    void main() {
+        Solution36 test = new Solution36();
+        String user = " ";
+    }
+}
+class Solution36Test4 {
+
+    @Test
+    void main() {
+        Solution36 test = new Solution36();
+        String user = "done";
+    }
+}
