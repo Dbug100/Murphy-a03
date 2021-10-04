6aea07440793ff3c5f33f2e83fd29af27e077d25
deaja <deaja214@gmail.com>
2021-10-03 21:14:13 -0400

    exercise 34 pseudocode

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,26 @@
+package baseline;
+
+public class Solution34 {
+    public static void main(String[] args){
+        //make list of employees
+
+        //call to print list
+
+        //ask for name of employee
+
+        /*
+            loop through array if
+            you reach name of employee
+            skip it every other name goes in new array
+         */
+
+        //call to printList
+    }
+    private static void printList(String[] names){
+        //find length of array
+
+        //print number of employees
+
+        //loop through and print out each name in array
+    }
+}
