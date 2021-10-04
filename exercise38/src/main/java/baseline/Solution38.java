b9962baf78cf611aeb37bd464ae8a0b12d975227
deaja <deaja214@gmail.com>
2021-10-04 16:30:24 -0400

    pseudocode for exercise 38

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,20 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Deaja Murphy
+ */
+
+public class Solution38 {
+    public static void main(String[] args){
+        // ask for list
+        //call filterEvenNumbers
+        //print
+    }
+    private static String[] filterEvenNumbers(String[] array){
+        //for length of array
+        //if array[i] % 2 = 0
+        //add to newList
+        //return new list;
+        return array;
+    }
+}
