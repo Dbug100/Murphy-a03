317ca256ea30f9ff7310cc378d7ed088e52bb540
deaja <deaja214@gmail.com>
2021-10-03 21:44:20 -0400

    exercise 35 pseudocode

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,23 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Deaja Murphy
+ */
+
+public class Solution35 {
+    public static void main(String[] args){
+        //initialize array
+
+        //while loop
+
+        //ask to enter name
+
+        //accept name until blank entry
+
+        //exit loop at blank entry
+
+        //generate random number between 0 and length of array
+
+        //print name at that index
+    }
+}
