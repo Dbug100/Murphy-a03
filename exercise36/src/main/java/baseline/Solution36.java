08c4141d346cd397a15ea3f4867a9de6f41a5330
deaja <deaja214@gmail.com>
2021-10-03 22:45:37 -0400

    exercise 36 psuedocode

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,56 @@
+package baseline;
+
+import java.util.ArrayList;
+
+public class Solution36 {
+    public static void main(String[] args){
+        //ask for numbers in a while loop
+
+        //if entry does not equal done NumberFormatException
+        //if not done and is a number add to list
+
+        //when user enters done exit loop
+
+        //call average
+        //call min
+        //call max
+        //call std
+        //call printOut(average, min, max, std)
+    }
+    private static double average(ArrayList<Integer> array){
+        //for length of list
+        //add each num together
+        //divide by size
+        //return
+        return 0;
+    }
+    private static int min(ArrayList<Integer> array){
+        // int = 0
+        //for length of array
+        // if int > array[index]
+        //int = array[index]
+        //return
+        return 0;
+    }
+    private static int max(ArrayList<Integer> array){
+        // int = 0
+        //for length of array
+        // if int < array[index]
+        //int = array[index]
+        //return
+        return 0;
+    }
+    private static double std(ArrayList<Integer> array){
+        // for length of array
+        // sum+= math.pow((array[index]-average),2)
+        //double stdv = Math.sqrt(sum/array size)
+        //return
+        return 0;
+    }
+    private static void printOut(double avg, int minimum, int maximum, double stdv){
+        //print avg
+        //print min
+        //print max
+        //print stdv
+    }
+}
