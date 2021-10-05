ea86d12281059fe287c35580f1ef3162fb6c1756
deaja <deaja214@gmail.com>
2021-10-04 21:37:30 -0400

    exercise 38 solution

--- exercise38/src/main/java/baseline/Solution38.java
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -4,17 +4,42 @@ package baseline;
  *  Copyright 2021 Deaja Murphy
  */
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Solution38 {
     public static void main(String[] args){
+        Scanner userIn = new Scanner(System.in);
         // ask for list
+        System.out.print("Enter a list of numbers, separated by spaces : ");
+        String user = userIn.nextLine();
+
         //call filterEvenNumbers
         //print
+        printList(filterEvenNumbers(user));
     }
-    private static String[] filterEvenNumbers(String[] array){
+    private static ArrayList<Integer> filterEvenNumbers(String list){
         //for length of array
-        //if array[i] % 2 = 0
-        //add to newList
+        String[] newList = list.split(" ");
+        ArrayList<Integer> evens = new ArrayList<>();
+        for (String s : newList) {
+            //if array[i] % 2 = 0
+            int num = Integer.parseInt(s);
+            if (num % 2 == 0) {
+                //add to newList
+                evens.add(num);
+            }
+        }
         //return new list;
-        return array;
+        return evens;
+    }
+    private static void printList(ArrayList<Integer> list){
+        System.out.print("The even numbers are: ");
+        for (int i = 0; i < list.size(); i++) {
+            if(i < list.size()-1)
+                System.out.print(list.get(i) +" ");
+            else
+                System.out.print(list.get(i) +".");
+        }
     }
 }
---
+++ exercise38/src/test/java/baseline/Solution38Test.java
@@ -0,0 +1,30 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution38Test {
+
+    @Test
+    void main() {
+        Solution38 test = new Solution38();
+        String user = ("1 2 4 6  8  9 13 15");
+    }
+}
+class Solution38Test2 {
+
+    @Test
+    void main() {
+        Solution38 test = new Solution38();
+        String user = (" ");
+    }
+}
+class Solution38Test3 {
+
+    @Test
+    void main() {
+        Solution38 test = new Solution38();
+        String user = ("1 6");
+    }
+}
